9c3f76f8a6984e26e9900bd417d599281705a68c
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-05 09:57:17 +0300

    Logic with buttons

--- lib/coffee_preps.dart
+++ lib/coffee_preps.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class Coffee extends StatelessWidget {
   const Coffee({super.key});
 
+  void increaseStrength() {
+    print("inc sterngth by 1");
+  }
+
+  void increaseSugar() {
+    print("increase sugar by 1");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,28 +19,35 @@ class Coffee extends StatelessWidget {
           children: [
             const Text('strength: '),
             const Text('3'),
-            Image.asset('assets/img/1.jpg',
-            width: 25,
-            color: Colors.white,
-            colorBlendMode: BlendMode.multiply,
+            Image.asset(
+              'assets/img/1.jpg',
+              width: 25,
+              color: Colors.white,
+              colorBlendMode: BlendMode.multiply,
             ),
-            const Expanded(child:SizedBox()),
-            FilledButton(onPressed: (){}, child: const Text("+")),
-            
+            const Expanded(child: SizedBox()),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseStrength, child: const Text("+")),
           ],
         ),
         Row(
-            children: [
+          children: [
             Text('sugar: '),
             Text('3'),
-            Image.asset('assets/img/2.jpg',
-            width: 25,
-            ),
-            const Expanded(child:SizedBox()),
-            TextButton(onPressed: (){}, child: const Text("+")),
-
+            Image.asset('assets/img/2.jpg', width: 25),
+            const Expanded(child: SizedBox()),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseSugar, child: const Text("+")),
           ],
-        )
+        ),
       ],
     );
   }
