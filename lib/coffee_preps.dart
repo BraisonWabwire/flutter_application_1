b0cab2fdb4ca1db5f2af8d07cf666efe0404a8ab
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-05 10:28:31 +0300

    control

--- lib/coffee_preps.dart
+++ lib/coffee_preps.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Coffee extends StatelessWidget {
+class Coffee extends StatefulWidget {
   const Coffee({super.key});
 
-  void increaseStrength() {
-    print("inc sterngth by 1");
-  }
+  @override
+  State<Coffee> createState() => _CoffeeState();
+}
 
-  void increaseSugar() {
-    print("increase sugar by 1");
+class _CoffeeState extends State<Coffee> {
+  int strength = 1;
+  int sugar = 1;
+
+  void increaseStrength() {
+    setState(() {
+      strength = strength < 5 ? strength + 1 : 1;
+    });
+    
   }
+   void increaseSugar() {
+      setState(() {
+        sugar = sugar < 5 ? sugar + 1 : 0;
+      });
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +29,8 @@ class Coffee extends StatelessWidget {
       children: [
         Row(
           children: [
-            const Text('strength: '),
-            const Text('3'),
+            Text('strength'),
+            for (int i=0; i< strength; i++)
             Image.asset(
               'assets/img/1.jpg',
               width: 25,
@@ -31,13 +43,17 @@ class Coffee extends StatelessWidget {
                 backgroundColor: Colors.brown,
                 foregroundColor: Colors.white,
               ),
-              onPressed: increaseStrength, child: const Text("+")),
+              onPressed: increaseStrength,
+              child: const Text("+"),
+            ),
           ],
         ),
         Row(
           children: [
             Text('sugar: '),
-            Text('3'),
+            if (sugar==0)
+            const Text("No sugars...."),
+            for(int i=0; i<sugar; i++)
             Image.asset('assets/img/2.jpg', width: 25),
             const Expanded(child: SizedBox()),
             FilledButton(
@@ -45,7 +61,9 @@ class Coffee extends StatelessWidget {
                 backgroundColor: Colors.brown,
                 foregroundColor: Colors.white,
               ),
-              onPressed: increaseSugar, child: const Text("+")),
+              onPressed: increaseSugar,
+              child: const Text("+"),
+            ),
           ],
         ),
       ],
